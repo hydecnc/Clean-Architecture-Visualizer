3bf612c3267e77cf248a3cc6a1dd0d966caf1577
Hyde Yoo <hydeyoo0716@gmail.com>
2026-09-05 16:28:47 -0400

    feat: add gts to backend

---
+++ clean-architecture-visualizer/eslint.ignores.cjs
@@ -0,0 +1,3 @@
+// frontend/ is not linted by gts at the moment.
+// Once the project is restructured to have a single package.json, this should be modified accordingly.
+module.exports = ['dist/', 'frontend/']
